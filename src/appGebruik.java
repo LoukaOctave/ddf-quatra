14a241a59570e0b5a78549f7420ba839ae0b24ea
LoukaOctave <louka.octave@student.odisee.be>
2021-03-15 11:41:17 +0100

    appGebruik uit het Domain package gehaald, hoorde daar niet te zijn

--- src/appGebruik.java
+++ src/appGebruik.java
@@ -1,10 +1,18 @@
-
+import Domain.Afspraak;
+import Domain.ProductkeuzeOld;
 
 public class appGebruik {
 
     public static void main(String[] args) {
 
+        Afspraak afspraak = new Afspraak();
 
+        ProductkeuzeOld productKeuze = new ProductkeuzeOld();
 
+        productKeuze.keuzeProduct();
+
+        afspraak.selecteerGewensteDatum();
+
+        afspraak.stuurDoorNaarPlanner();
     }
 }
